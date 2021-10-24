c029d47d0bebd2eaca020f3db7e2fa69a7a27bd8
jisuoh <oh.suroc@gmail.com>
2021-10-24 22:41:20 +0900

    2.7 Nicknames

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,8 +1,11 @@
 const socket = io();
 
 const welcome = document.getElementById("welcome");
+const roomForm = welcome.querySelector("#roomname");
+const nameForm = welcome.querySelector("#name");
+
 const room = document.getElementById("room");
-const form = welcome.querySelector("form");
+
 
 room.hidden = true;
 let roomName;
@@ -15,7 +18,7 @@ function addMessage(message) {
 }
 function handleMessageSubmit(event) {
     event.preventDefault();
-    const input = room.querySelector("input");
+    const input = room.querySelector("#msg input");
     const value = input.value;
     socket.emit("new_message", input.value, roomName, ()=> {
         addMessage(`You: ${value}`);
@@ -23,30 +26,40 @@ function handleMessageSubmit(event) {
     input.value = "";
 }
 
+function handleNicknameSubmit(event) {
+    event.preventDefault();
+    const input = nameForm.querySelector("input");
+    socket.emit("nickname", input.value);
+}
+
 function showRoom() {
     welcome.hidden = true;
     room.hidden = false;
+
     const h3 = room.querySelector("h3");
     h3.innerText = `Room ${roomName}`;
-    const form = room.querySelector("form");
-    form.addEventListener("submit", handleMessageSubmit);
+    const msgForm = room.querySelector("#msg");
+    msgForm.addEventListener("submit", handleMessageSubmit);
+
 }
+
+nameForm.addEventListener("submit", handleNicknameSubmit);
 
 function handleRoomSubmit(event) {
     event.preventDefault();
-    const input = form.querySelector("input");
+    const input = roomForm.querySelector("input");
     socket.emit("enter_room", input.value, showRoom);
     roomName = input.value;
     input.value = "";
 }
-form.addEventListener("submit", handleRoomSubmit);
+roomForm.addEventListener("submit", handleRoomSubmit);
 
-socket.on("welcome", ()=> {
-    addMessage("Someone joined!");
+socket.on("welcome", (user) => {
+    addMessage(`${user} arrived!`);
 });
 
-socket.on("bye", ()=> {
-    addMessage("Somone left U.U");
+socket.on("bye", (left) => {
+    addMessage(`${left} left U.U`);
 })
 
 socket.on("new_message", addMessage);
