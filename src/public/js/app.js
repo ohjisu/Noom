f46df3c91285be6ea6147c8ff90f91857ef7d214
jisuoh <oh.suroc@gmail.com>
2021-10-25 10:44:21 +0900

    2.8~2.9 Room Count 2.10 User Count

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,11 +1,10 @@
 const socket = io();
 
 const welcome = document.getElementById("welcome");
-const roomForm = welcome.querySelector("#roomname");
-const nameForm = welcome.querySelector("#name");
-
 const room = document.getElementById("room");
 
+const roomForm = welcome.querySelector("#roomname");
+const nameForm = welcome.querySelector("#name");
 
 room.hidden = true;
 let roomName;
@@ -54,12 +53,32 @@ function handleRoomSubmit(event) {
 }
 roomForm.addEventListener("submit", handleRoomSubmit);
 
-socket.on("welcome", (user) => {
+socket.on("welcome", (user, newCount) => {
+    const h3 = room.querySelector("h3");
+    h3.innerText = `Room ${roomName} (${newCount})`;
+
     addMessage(`${user} arrived!`);
 });
 
-socket.on("bye", (left) => {
+socket.on("bye", (left, newCount) => {
+    const h3 = room.querySelector("h3");
+    h3.innerText = `Room ${roomName} (${newCount})`;
+
     addMessage(`${left} left U.U`);
 })
 
 socket.on("new_message", addMessage);
+
+socket.on("room_change", (rooms)=> {
+    const roomList = welcome.querySelector("ul");
+    roomList.innerHTML = "";
+    if(rooms.length === 0) {
+        return;
+    }
+
+    rooms.forEach((room)=> {
+        const li = document.createElement("li");
+        li.innerText = room;
+        roomList.append(li);
+    })
+});
--- src/server.js
+++ src/server.js
@@ -14,22 +14,45 @@ app.get("/*", (_, res) => res.redirect("/"));
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
+function publicRooms() {
+    const {
+        sockets: {
+            adapter: {sids, rooms}
+        }
+    } = wsServer;
+
+    const publicRooms = [];
+    rooms.forEach((_, key)=> {
+        if ( sids.get(key) === undefined ) publicRooms.push(key)
+    });
+    return publicRooms;
+}
+
+function countRoom(roomName) {
+    return wsServer.sockets.adapter.rooms.get(roomName)?.size;
+}
+
 wsServer.on("connection", (socket)=> {
     socket["nickname"] = "Anon";
 
     socket.onAny((event)=> {
+        console.log(wsServer.sockets.adapter); // in-memory Adapter
         console.log(`Socket Event:${event}`);
     });
     socket.on("enter_room", (roomName, done)=> {
         socket.join(roomName);
         done();
-        socket.to(roomName).emit("welcome", socket.nickname);
+        socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));
+        wsServer.sockets.emit("room_change", publicRooms());
     });
     socket.on("disconnecting", ()=> {
         socket.rooms.forEach((room)=>{
-            socket.to(room).emit("bye", socket.nickname);
-        })
+            socket.to(room).emit("bye", socket.nickname, countRoom(room)-1);
+        });
     });
+    socket.on("disconnect", ()=> {
+        wsServer.sockets.emit("room_change", publicRooms());
+    })
     socket.on("new_message", (msg, room, done)=> {
         socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
         done();
@@ -42,3 +65,6 @@ wsServer.on("connection", (socket)=> {
 const handleListen = () => console.log(`listening on http://localhost:5000`);
 
 httpServer.listen(5000, handleListen);
+
+// Adapter : 다른 서버들 사이에서 실시간으로 어플리케이션을 동기화. 서버의 메모리에서 Adapter를 사용.
+// 누가 접속중인지, room 개수, 등을 알려줌
