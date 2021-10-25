481184b55294429168e50b673ce7a8f14b5a5946
jisuoh <oh.suroc@gmail.com>
2021-10-25 17:38:01 +0900

    3.5 Offers 3.6 Answers 3.7 IceCandidate

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -11,6 +11,8 @@ let myStream;
 let muted = false;
 let cameraOff = false;
 let roomName;
+let myPeerConnection;
+
 
 const call     = document.getElementById("call");
 call.hidden = true;
@@ -100,25 +102,77 @@ camerasSelect.addEventListener("input", handleCameraChange);
 const welcome  = document.getElementById("welcome");
 const welcomeForm = welcome.querySelector("form");
 
-function startMedia() {
+async function initCall() {
     welcome.hidden = true;
     call.hidden = false;
-    getMedia();
+    await getMedia();
+    makeConnection();
 }
 
-function handleWelcomeSubmit(event) {
+async function handleWelcomeSubmit(event) {
     event.preventDefault();
     const input = welcomeForm.querySelector("input");
     const value = input.value;
-    socket.emit("join_room", value, startMedia);
+
+    await initCall();
+    socket.emit("join_room", value);
     roomName = value;
     input.value = "";
 }
 
 welcomeForm.addEventListener("submit", handleWelcomeSubmit);
 
+
+
 // Socket code *********************************************************************************************************
 
-socket.on("welcome", ()=> {
-    console.log("someone joined");
+socket.on("welcome", async ()=> {
+    const offer = await myPeerConnection.createOffer(); // (3) Peer A : createOffer
+    myPeerConnection.setLocalDescription(offer);        // (4) Peer A : setLocalDescription
+    console.log("send the offer");
+    socket.emit("offer", offer, roomName);              // Peer A -> Server
 });
+
+socket.on("offer", async (offer)=> {
+    console.log("received the offer");
+    // Peer B : received Peer A's offer
+    myPeerConnection.setRemoteDescription(offer);           // (5) Peer B : setRemoteDescription
+    const answer = await myPeerConnection.createAnswer();   // (8) Peer B : createAnswer
+    myPeerConnection.setLocalDescription(answer);           // (9) Peer B : setLocalDescription
+    socket.emit("answer", answer, roomName);                // Peer B -> Server
+    console.log("send the answer");
+})
+
+socket.on("answer", async (answer)=> {
+    console.log("received the answer");
+    myPeerConnection.setRemoteDescription(answer);          // (10) Peer B : setRemoteDescription
+});
+
+socket.on("ice", ice=> {
+    console.log("received candidate");
+    myPeerConnection.addIceCandidate(ice);                  // (12) Peer B, Peer A : addIcdCandidate
+})
+
+
+// WebRTC code *********************************************************************************************************
+
+function makeConnection() {
+    myPeerConnection = new RTCPeerConnection(); // create peer-to-peer connection
+    myPeerConnection.addEventListener("icecandidate", handleIce);   // (11) Peer A: Event Listener("icecandidate")
+    myPeerConnection.addEventListener("addstream", handleAddStream);
+
+    // audio and video data stream 을 connection에 추가.
+    myStream.getTracks().forEach((track)=> {
+        myPeerConnection.addTrack(track, myStream);     // (2) Peer A : addTrack(addStream)
+    });
+}
+
+function handleIce(data) {
+    console.log("send candidate");
+    socket.emit("ice", data.candidate, roomName);
+}
+
+function handleAddStream(data) {
+    const peerFace = document.getElementById("peerFace");
+    peerFace.srcObject = data.stream;
+}
